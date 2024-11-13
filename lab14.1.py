3e0a6e25ff98b17d2c107d1b454270d81d1e9152
SophiaBaranova <147512154+SophiaBaranova@users.noreply.github.com>
2024-11-13 19:25:16 +0200

    завдання 1

---
+++ lab14.1.py
@@ -0,0 +1,26 @@
+﻿import matplotlib.pyplot as plt
+import numpy as np
+
+#інтервал для x
+x = np.linspace(-2, 2, 100) 
+
+#функція
+y = 1/x * np.sin(x) * np.cos(x**2 + 1/x)
+
+#створення графіку
+plt.plot(x, y, label=r'$\frac{1}{x}\bullet\sin(x)\bullet\cos(x^{2}+\frac{1}{x})$', color = "red", linewidth = 2)
+#назва графіку
+plt.title('Графік функції', fontsize=15) 
+#позначення вісі х
+plt.xlabel('x', fontsize=12, color='red') 
+#позначення вісі у
+plt.ylabel('y', fontsize=12, color='red') 
+#позначення легенди
+plt.legend()
+#позначення сітки
+plt.grid()
+
+#збереження
+plt.savefig("1.png")
+#відображення
+plt.show()
